f1b3eaf1ac1e89ee6916e7cd2b75738f727fdb46
Ugagguturu <134679802+Ugagguturu@users.noreply.github.com>
2025-04-10 17:50:12 +0530

    Create variables.tf

---
+++ vm/variables.tf
@@ -0,0 +1,21 @@
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "East US"
+}
+
+variable "admin_username" {
+  description = "Admin username"
+  type        = string
+}
+
+variable "admin_password" {
+  description = "Admin password"
+  type        = string
+  sensitive   = true
+}
