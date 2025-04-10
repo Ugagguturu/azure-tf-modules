c9388dcfb50aba361fccdbae621ebfa0d55d3c2d
Ugagguturu <134679802+Ugagguturu@users.noreply.github.com>
2025-04-10 18:51:42 +0530

    Update main.tf

--- vm/main.tf
+++ vm/main.tf
@@ -32,16 +32,9 @@ resource "azurerm_network_interface" "nic" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
 }
 
-resource "azurerm_public_ip" "public_ip" {
-  name                = "myPublicIP"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
-}
 
 resource "azurerm_windows_virtual_machine" "vm" {
   name                = "myVM"
